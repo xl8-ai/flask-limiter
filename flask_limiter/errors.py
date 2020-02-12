f5ca18812c0441a4ec474216cbe5c2348fd867b1
Jay Jinhyung Park <jay@xl8.ai>
2020-02-11 17:09:51 -0800

    Fix AWS Lambda serverless deployment issue
    raise DistributionNotFound(req, requirers)
    pkg_resources.DistributionNotFound: The 'werkzeug' distribution was not found and is required by the application

--- flask_limiter/errors.py
+++ flask_limiter/errors.py
@@ -7,16 +7,7 @@ from pkg_resources import get_distribution
 from six import text_type
 from werkzeug import exceptions
 
-werkzeug_exception = None
-werkzeug_version = get_distribution("werkzeug").version
-if LooseVersion(werkzeug_version) < LooseVersion("0.9"):  # pragma: no cover
-    # sorry, for touching your internals :).
-    import werkzeug._internal
-    werkzeug._internal.HTTP_STATUS_CODES[429] = 'Too Many Requests'
-    werkzeug_exception = exceptions.HTTPException
-else:
-    # Werkzeug 0.9 and up have an existing exception for 429
-    werkzeug_exception = exceptions.TooManyRequests
+werkzeug_exception = exceptions.TooManyRequests
 
 
 class RateLimitExceeded(werkzeug_exception):
